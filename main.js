2da049dd02a3b33760e9b6dfbde6af4e204064ac
Nuria Medina <nmedinaarnaz@gmail.com>
2023-07-21 15:14:54 -0700

    first commit

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ let typewriter = new Typewriter(app, {
  
 typewriter
   .pauseFor(2500)
-  .typeString('Desarrollo sitios web y escribo artículos sobre programación.')
+  .typeString('Soy estudiante de neurociencias y estoy aprendiendo programación y desarrollo de sitios web.')
   .pauseFor(200)
   .deleteChars(10)
   .start();
